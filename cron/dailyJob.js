30816298df547e1755f15dd430fcea7a73e2ae63
Rj <rahulsingh85064@gmail.com>
2026-02-10 18:58:50 +0530

    Add timezone in dailyjob

--- cron/dailyJob.js
+++ cron/dailyJob.js
@@ -3,10 +3,10 @@ import cron from 'node-cron'
 import { sendDailySummary } from '../controllers/whatsAppController.js'
 
 export const scheduleDailyJob = () => {
-cron.schedule("0 19 * * *", async () => {
-  console.log('Running daily WhatsApp summary...');    
-  await sendDailySummary()
-
-})
-
-}
+  cron.schedule("1 19 * * *", async () => {
+    console.log('Running daily WhatsApp summary...');
+    await sendDailySummary();
+  }, {
+    timezone: "Asia/Kolkata"
+  });
+};
