97bbb92bef597748ecf35e4d522dcd5ddce1409d
Rj <rahulsingh85064@gmail.com>
2026-02-27 23:00:34 +0530

    update cron time

--- cron/dailyJob.js
+++ cron/dailyJob.js
@@ -3,7 +3,7 @@ import cron from 'node-cron'
 import { sendDailySummary } from '../controllers/whatsAppController.js'
 
 export const scheduleDailyJob = () => {
-  cron.schedule("53 22 * * *", async () => {
+  cron.schedule("0 8 * * *", async () => {
     console.log('Running daily WhatsApp summary...');
     await sendDailySummary();
   }, {
